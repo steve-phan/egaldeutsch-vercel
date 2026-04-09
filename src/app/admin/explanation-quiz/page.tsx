458075f1f31b610f0f99521a7494cf172c290a00
steve-phan <phanhaingoc@gmail.com>
2026-04-09 21:32:21 +0200

    Remove Explanation Quiz, lesson management, and blog components along with related functionality

--- src/app/admin/explanation-quiz/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ExplanationQuizAdmin } from "@/components/admin/explanation-quiz-admin";
-
-export default function AdminExplanationQuizPage() {
-  return (
-    <div className="container mx-auto px-4 py-8 max-w-6xl">
-      <ExplanationQuizAdmin />
-    </div>
-  );
-}
